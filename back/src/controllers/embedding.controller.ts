deda1b6d2810496c3bff40f1672898fc2e53ed68
LucianoMazar <lmazaraojr@gmail.com>
2026-02-26 20:53:36 -0300

    feat: enchanced error handler

--- back/src/controllers/embedding.controller.ts
+++ back/src/controllers/embedding.controller.ts
@@ -1,16 +1,26 @@
 import { embeddingServices } from "../services/embedding.service.js"
 import type { Request, Response } from "express";
+import { AppError } from "../errors/appError.js";
 
 export const embeddingCotroller = {
-    async reciveSentences(req: Request, res: Response){
-        try{
-            if(!req.params.sentences) throw new Error("no sentences provided")
+  async reciveSentences(req: Request, res: Response) {
+    try {
+      const { sentences } = req.body;
 
-            const sentences = req.params.sentences
-            const data = await embeddingServices.embedData(sentences)
-            res.json(data)
-        }catch(e){
-            res.status(400).send({error: "bad request"})
-        }
+      if (!sentences) {
+        throw new AppError("No sentences provided", 422);
+      }
+
+      const data = await embeddingServices.embedData(sentences);
+
+      return res.status(200).json(data);
+
+    } catch (e: any) {
+      if (e instanceof AppError) {
+        return res.status(e.statusCode).json({ error: e.message });
+      }
+
+      return res.status(500).json({ error: "Internal server error" });
     }
-}
+  }
+};
