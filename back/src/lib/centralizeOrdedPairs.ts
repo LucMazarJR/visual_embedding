1dd07e6e4ff9bc72475d7ba25d45d9527a29e47b
LucianoMazar <lmazaraojr@gmail.com>
2026-02-27 11:35:20 -0300

    feat: add center function to orded pairs

---
+++ back/src/lib/centralizeOrdedPairs.ts
@@ -0,0 +1,24 @@
+export function center(points: number[][] | null | undefined): number[][] {
+  if (!points || points.length === 0) return []
+
+  const validPoints = points.filter(
+    (p): p is [number, number] =>
+      Array.isArray(p) &&
+      p.length === 2 &&
+      typeof p[0] === "number" &&
+      typeof p[1] === "number"
+  )
+
+  //Tratamento de erros
+
+  const meanX =
+    validPoints.reduce((sum, p) => sum + p[0], 0) / validPoints.length
+
+  const meanY =
+    validPoints.reduce((sum, p) => sum + p[1], 0) / validPoints.length
+
+  return validPoints.map(([x, y]) => [
+    x - meanX,
+    y - meanY
+  ])
+}
--- back/src/services/embedding.service.ts
+++ back/src/services/embedding.service.ts
@@ -1,11 +1,13 @@
 import { applyUmap } from "../lib/applyUMAP.js"
 import { embedSentences } from "../lib/embedSentences.js"
+import { center } from "../lib/centralizeOrdedPairs.js"
 
 export const embeddingServices = {
-    async embedData(sentences: string[] | string) {
-        const result = await embedSentences(sentences)
-        const embeddings = result.embeddings?.map(vector => vector.values).filter((v): v is number[] => v !== undefined) || []
-        const reducedData = applyUmap(embeddings)
-        return reducedData
-    }
+  async embedData(sentences: string[] | string) {
+    const result = await embedSentences(sentences)
+    const embeddings = result.embeddings?.map(vector => vector.values).filter((v): v is number[] => v !== undefined) || []
+    const reducedData = applyUmap(embeddings)
+    const centeredData = center(reducedData)
+    return centeredData
+  }
 }
