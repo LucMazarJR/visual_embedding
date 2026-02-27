92b572e7433439335ded3cf67cd8fb971bf8275a
LucianoMazar <lmazaraojr@gmail.com>
2026-02-26 21:33:25 -0300

    feat: creat PCA script

--- back/src/services/embedding.service.ts
+++ back/src/services/embedding.service.ts
@@ -1,7 +1,11 @@
 import { embedSentences } from "../lib/embedSentences.js"
+import { applyPCA } from "../lib/applyPca.js"
 
 export const embeddingServices = {
     async embedData(sentences: string[] | string) {
-        return embedSentences(sentences)
+        const result = await embedSentences(sentences)
+        const embeddings = result.embeddings?.map(vector => vector.values).filter((v): v is number[] => v !== undefined) || []
+        const reducedData = applyPCA(embeddings, 2)
+        return reducedData
     }
 }
