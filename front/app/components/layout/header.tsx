745b323413aae14f45600b18feb8b91d368e0235
LucianoMazar <lmazaraojr@gmail.com>
2026-03-09 19:06:05 -0300

    feat: header links working

--- front/app/components/layout/header.tsx
+++ front/app/components/layout/header.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import LanguageButton from "./language_button";
 
 export default function Header() {
     return (
         <header className="flex justify-between py-8 px-6">
-            <nav className="flex gap-12">
-                <a href="">HOME</a>
-                <a href="">ABOUT</a>
+            <nav>
+                <ul className="flex gap-12 items-center">
+                    <li>
+                        <Link href={"/"}>HOME</Link>
+                    </li>
+                    <li>
+                        <Link href={"/about"}>ABOUT</Link>
+                    </li>
+                </ul>
             </nav>
             <LanguageButton/>
         </header>
