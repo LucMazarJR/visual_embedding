2282be9cd24937f48f196b89dd5bf0828978504e
LucianoMazar <lmazaraojr@gmail.com>
2026-03-10 20:17:23 -0300

    style: remove not used system colors in global css

--- front/app/_components/layout/header.tsx
+++ front/app/_components/layout/header.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
 import LanguageButton from "./language_button";
 import NavBar from "./nav";
 
 export default function Header() {
     return (
         <header className="flex justify-between py-8 px-6">
-            <NavBar/>
-            <LanguageButton/>
+            <Link href={"/"} className="font-bold text-4xl">Sentence Space</Link>
+            <div className="flex gap-8">
+                <NavBar />
+                <LanguageButton />
+            </div>
         </header>
     )
 }
