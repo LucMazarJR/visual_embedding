848bfa0427dee9896da30584fdfb6d3e6ff2f4ae
LucianoMazar <lmazaraojr@gmail.com>
2026-03-12 11:22:50 -0300

    feat: init decoration graphic in hero section

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,11 @@
+import { Box } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2 p-8">
-      <div className="flex flex-col justify-center">
-        <div className="h-full px-20 space-y-12">
+    <div className="grid grid-cols-2 p-8 flex-1">
+      <div className="flex flex-col justify-center items-center h-full">
+        <div className="px-20 space-y-12">
           <h1 className="font-black tracking-wider text-7xl">Bem vindo à Sentence Space</h1>
           <p className="text-xl">Explore o conceito de vetores semânticos e vizualize como a inteligência artificial processa o significado da linguagem em dimensões matemáticas</p>
           <div className="space-x-4">
@@ -17,9 +18,13 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="px-8">
-        a
+      <div className="flex flex-col items-center justify-center">
+        <div className="bg-[url('/frame1.svg')] bg-repeat flex items-center justify-center w-lg h-128 border border-gray-300 rounded-2xl shadow-2xl">
+          <div className="border-5 w-fit h-fit rounded-full border-dashed border-gray-200 animate-spin [animation-duration:10s]">
+            <Box className="w-42 h-42 m-20 animate-spin [animation-duration:10s] [animation-direction:reverse] border border-gray-700 rounded-full p-12 shadow-2xl" color="#99a1af"/>
+          </div>
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
