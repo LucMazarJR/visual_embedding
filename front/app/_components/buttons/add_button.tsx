9c89a3d9540a60a3d77a838626cb491befd382fc
LucianoMazar <lmazaraojr@gmail.com>
2026-03-18 11:55:48 -0300

    style: add shadow to workspace buttons

--- front/app/_components/buttons/add_button.tsx
+++ front/app/_components/buttons/add_button.tsx
@@ -9,7 +9,7 @@ export default function AddButton({ addFunc, len }: AddButtonProps) {
   if (len < 10) {
     return (
       <button
-        className="bg-white border-dashed border-2 p-2 py-4 rounded-lg border-gray-300 font-medium flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-50"
+        className="bg-white border-dashed border-2 p-2 py-4 rounded-lg border-gray-300 font-medium flex justify-center items-center gap-2 cursor-pointer hover:bg-gray-50 shadow-lg"
         type="button"
         onClick={addFunc}
       >
--- front/app/_components/buttons/generate_button.tsx
+++ front/app/_components/buttons/generate_button.tsx
@@ -2,7 +2,7 @@ import { ChartSpline } from "lucide-react";
 
 export default function GenerateButton() {
     return(
-        <button className="bg-foreground hover:bg-accent-foreground p-2 py-4 rounded-lg text-white cursor-pointer flex justify-center gap-2" type="button">
+        <button className="bg-foreground hover:bg-accent-foreground p-2 py-4 rounded-lg text-white cursor-pointer flex justify-center gap-2 shadow-lg" type="button">
             <ChartSpline/>Gerar Vizualização            
         </button>
     )
