5ec90c73de94b0180659c3c27607500b6f5a5e52
LucianoMazar <lmazaraojr@gmail.com>
2026-03-19 21:05:28 -0300

    feat: add phrases limit in front end

--- front/app/_components/buttons/add_button.tsx
+++ front/app/_components/buttons/add_button.tsx
@@ -1,4 +1,4 @@
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, X } from "lucide-react";
 
 type AddButtonProps = {
   addFunc: () => void;
@@ -6,18 +6,14 @@ type AddButtonProps = {
 };
 
 export default function AddButton({ addFunc, len }: AddButtonProps) {
-  if (len < 10) {
-    return (
-      <button
-        className="w-full flex cursor-pointer items-center justify-center gap-2 rounded-lg border-2 border-dashed border-gray-300 bg-white p-2 py-4 font-medium shadow-lg hover:bg-gray-50"
-        type="button"
-        onClick={addFunc}
-      >
-        <CirclePlus />
-        Adicionar Frase
-      </button>
-    );
-  }
-
-  return null;
+  return (
+    <button
+      className={"w-full flex items-center justify-center gap-2 rounded-lg border-2 border-gray-300 p-2 py-4 font-medium shadow-lg transition-colors ease-in-out duration-150" + ((len >= 10) ? " bg-gray-200 cursor-not-allowed" : " hover:bg-gray-50 border-dashed cursor-pointer")}
+      type="button"
+      onClick={addFunc}
+    >
+      {len >= 10 ? <X/> : <CirclePlus />}
+      {len >= 10 ? ("Limite de frases atingido") : "Adicione uma Frase"}
+    </button>
+  );
 }
